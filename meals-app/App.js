8e173fcfeced2a926c223e8fe58058620a35c26a
vivekokta <vivek.giri@okta.com>
2025-03-23 12:15:20 +0530

    Drawer Added

--- meals-app/App.js
+++ meals-app/App.js
@@ -1,20 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavouriteScreen from './screens/FavouriteScreen';
 
-const myStack = createNativeStackNavigator();
+const MyStack = createNativeStackNavigator();
+const MyDrawer = createDrawerNavigator();
+
+function MyDrawerNavigator() {
+  return (
+    <MyDrawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#561D22FF' },
+        headerTitleAlign: 'center',
+        headerTintColor: 'white',
+        sceneStyle: {
+          backgroundColor: '#4C0A10FF',
+        },
+        drawerContentStyle: {
+          backgroundColor: '#4D1C20FF',
+        },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#390409FF',
+        drawerActiveBackgroundColor: '#B6868AFF',
+        animationMatchesGesture: true,
+      }}
+    >
+      <MyDrawer.Screen
+        name='MealsCategories'
+        component={CategoriesScreen}
+        options={{ title: 'All Categories', drawerIcon: ({ color, size }) => <Ionicons name='list' color={color} size={size} /> }}
+      />
+      <MyDrawer.Screen
+        name='Favourites'
+        component={FavouriteScreen}
+        options={{ title: 'My Favourites', drawerIcon: ({ color, size }) => <Ionicons name='star' color={color} size={size} /> }}
+      />
+    </MyDrawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
       <StatusBar style='light' />
       <NavigationContainer>
-        <myStack.Navigator
+        <MyStack.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: '#561D22FF' },
             headerTitleAlign: 'center',
@@ -23,10 +62,11 @@ export default function App() {
               backgroundColor: '#4C0A10FF',
             },
             animationMatchesGesture: true,
+            headerBackButtonDisplayMode: 'minimal' 
           }}
         >
-          <myStack.Screen name='MealsCategories' component={CategoriesScreen} options={{ title: 'All Categories' }} />
-          <myStack.Screen
+          <MyStack.Screen name='DrawerScreen' component={MyDrawerNavigator} options={{ headerShown: false }} />
+          <MyStack.Screen
             name='MealsOverview'
             component={MealsOverviewScreen}
             // options={({ route, navigation }) => {
@@ -34,8 +74,8 @@ export default function App() {
             //   return { title: catId };
             // }}
           />
-          <myStack.Screen name='MealDetail' component={MealDetailScreen} />
-        </myStack.Navigator>
+          <MyStack.Screen name='MealDetail' component={MealDetailScreen} />
+        </MyStack.Navigator>
       </NavigationContainer>
     </>
   );
