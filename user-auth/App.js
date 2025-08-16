e7385be159fbc66d59422f1c00030c7bf16f1a1c
vivekokta <vkgiri11@gmail.com>
2025-08-16 21:47:41 +0530

    minor fixes

--- user-auth/App.js
+++ user-auth/App.js
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
@@ -53,9 +52,7 @@ const App = () => {
       <StatusBar style='light' />
       <Navigation />
     </>
-  )
-}
+  );
+};
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
